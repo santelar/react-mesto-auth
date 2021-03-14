e1afcb5854399e61a59b501133c33a3f0f6fe96c
Alexander Ponomarenko <madi-al@yandex.ru>
2021-03-14 13:43:45 +0300

    refactor: header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import headerLogo from '../images/header__logo.svg';
 
-function Header() {
+function Header({ loggedIn, loginText, onClick, link, login }) {
   return (
     <header className="header">
       <img src={headerLogo} alt="Лого" className="header__logo" />
-      <NavLink exact to="/" className="header__button">Выйти</NavLink>
-      <NavLink to="/sign-in" className="header__button">Зарегистрироваться</NavLink>
-      <NavLink to="/sign-up" className="header__button">Войти</NavLink>
+      <div className="header__container">
+                <p className="header__email">{login}</p>
+                <Link to={link} className={`${loggedIn ? "header__link_singout" : ""} header__link`} onClick={onClick}>{loginText}</Link>
+            </div>
     </header>
   );
 }
